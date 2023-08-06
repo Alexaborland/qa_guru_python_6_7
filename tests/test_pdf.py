f9ac10060efd296693828c3a007fe0e5bf898114
Aleksandra Kashina <Borland3711@gmail.com>
2023-08-06 20:01:55 +0300

    the 5th push

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -5,7 +5,7 @@ from .conftest import resources_path
 
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 def test_by_pdf():
-    reader = PdfReader(os.path.join(resources_path, "docs-pytest-org-en-latest.pdf"))
+    reader = PdfReader(os.path.join(resources_path, 'docs-pytest-org-en-latest.pdf'))
     number_of_pages = len(reader.pages)
     page = reader.pages[0]
     text = page.extract_text()
@@ -14,4 +14,4 @@ def test_by_pdf():
     print(text)
 
     assert 412 == number_of_pages
-    assert 'to' in text
+    assert 'trainer' in text
