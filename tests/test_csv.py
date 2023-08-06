45430d3dbc60f6faf2a9799aa7808e6271f0b5db
Aleksandra Kashina <Borland3711@gmail.com>
2023-08-06 20:01:55 +0300

    the 5th push

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -6,13 +6,14 @@ from .conftest import resources_path
 
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 def test_by_csv():
-    with open(os.path.join(resources_path, 'resources/username.csv'), 'w') as csvfile:
+    with open(os.path.join(resources_path, 'username.csv'), 'w') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
         csvwriter.writerow(['Anna', 'Pavel', 'Peter'])
         csvwriter.writerow(['Alex', 'Serj', 'Yana'])
 
-    with open(os.path.join(resources_path, 'resources/username.csv')) as csvfile:
+    with open(os.path.join(resources_path, 'username.csv')) as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
             print(row)
-    assert len(row) == 3
+
+    assert os.path.exists(os.path.join(resources_path, 'username.csv'))
